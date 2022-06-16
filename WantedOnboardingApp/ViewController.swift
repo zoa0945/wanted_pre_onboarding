8c44f4ea3f85ae6d02e88bba19da2dbc3faa925c
Namhoon Jeong <zoa0945@gmail.com>
2022-06-16 18:19:37 +0900

    Initial Commit

---
+++ WantedOnboardingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedOnboardingApp
+//
+//  Created by Mac on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
